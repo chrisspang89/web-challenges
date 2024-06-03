b8fcf7bf6d1536578750d61f95b2e91bad088101
Christian <spang.christian@yahoo.de>
2024-06-03 10:55:49 +0200

    add

---
+++ react-data-fetching_iss-tracker/components/ISSTracker/index.js
@@ -0,0 +1,39 @@
+import Controls from "../Controls/index";
+import Map from "../Map/index";
+import useSWR from "swr";
+
+const URL = "https://api.wheretheiss.at/v1/satellites/25544";
+
+const fetcher = async (url) => {
+  const res = await fetch(url);
+  if (!res.ok) {
+    const error = new Error("An error occurred while fetching the data.");
+    error.info = await res.json();
+    error.status = res.status;
+    throw error;
+  }
+  return res.json();
+};
+
+export default function ISSTracker() {
+  const {
+    data: coords,
+    isLoading,
+    error,
+    mutate,
+  } = useSWR(URL, fetcher, { refreshInterval: 5000 });
+
+  if (isLoading || !coords) return <p>Loading...</p>;
+  if (error) return <p>Something went wrong...</p>;
+
+  return (
+    <main>
+      <Map longitude={coords.longitude} latitude={coords.latitude} />
+      <Controls
+        longitude={coords.longitude}
+        latitude={coords.latitude}
+        onRefresh={() => mutate()}
+      />
+    </main>
+  );
+}
